74e469fcc4e30a57ae7173908ed4124dfe2322bf
r_g7d <danangr123@gmail.com>
2023-06-25 10:58:01 +0700

    init

---
+++ src/data/info.ts
@@ -0,0 +1,88 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Danang Rahmat Bachtiar",
+  jobDescription: "Mobile Application Developer",
+  about: `I'm a passionate Mobile Application Developer with a knack for creating user-friendly and innovative mobile apps. With expertise in Flutter, I've developed apps for both Android and iOS platforms.
+  
+  Collaboration is my forte. Whether working with a team or independently, I thrive on delivering top-notch results that meet project objectives and timelines. I stay updated with the latest trends and technologies in mobile app development to ensure I bring fresh ideas to the table.
+  
+  I love the impact mobile apps can have on people's lives, simplifying tasks and enhancing experiences. Creating seamless and enjoyable app experiences is my driving force as a developer.
+  
+  Outside of coding, I enjoy staying connected with the tech community, attending conferences, and exploring new technologies.
+  
+  If you're looking for a dedicated and creative Mobile Application Developer, let's connect! Together, we can create exceptional mobile experiences.`,
+
+  experience: [
+    {
+      name: "Mobile Application Developer, PT. Inovasi Utama Nusantara",
+      location: "Malang, Indonesia",
+      startDate: "Dec 2022",
+      endDate: "Present",
+      description: [
+        "- Collaborated with cross-functional teams to gather requirements and develop mobile applications for Android and iOS platforms.",
+        "- Conducted thorough testing and debugging to identify and fix issues, ensuring high-quality and bug-free mobile applications.",
+        "- Maintained and updated existing mobile applications, incorporating new features and improvements based on user feedback.",
+        "- Ensured code quality and best practices by following coding standards, conducting code reviews, and implementing version control systems."
+      ],
+    },
+
+    {
+      name: "Mobile Application Developer, PUI-PT DLI",
+      location: "Malang, Indonesia",
+      startDate: "Apr 2022",
+      endDate: "Present",
+      description: [
+        "- Collaborated with cross-functional teams to gather requirements and develop mobile applications for Android and iOS platforms.",
+        "- Implemented user-friendly interfaces, ensuring a seamless and intuitive user experience.",
+        "- Conducted thorough testing and debugging to identify and fix issues, ensuring high-quality and bug-free mobile applications.",
+        "- Ensured code quality and best practices by following coding standards, conducting code reviews, and implementing version control systems."
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Informatics Engineering, State University of Malang",
+      location: "Malang, Indonesia",
+      startDate: "2021",
+      endDate: "2025",
+      description: [],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/r-G7D",
+    email: "danangrahmatbachtiar@gmail.com",
+    linkedin: "https://www.linkedin.com/in/danang-rahmat-bachtiar/",
+  },
+
+  projects: [
+    {
+      title: "Assessment Link",
+      isFeatured: true,
+      thumbnail: "/assets/images/asslink.png",
+      liveUrl: "https://play.google.com/store/apps/details?id=com.unesa.assessmentlink",
+    },
+    {
+      title: "Gamifikasi Kosakata Metode Penelitian",
+      isFeatured: true,
+      thumbnail: "/assets/images/gami.png",
+    },
+    {
+      title: "Ailment Alleviate",
+      isFeatured: true,
+      thumbnail: "/assets/images/ailment.png",
+      githubUrl: "https://github.com/r-G7D/ailment_alleviate",
+    },
+    {
+      title: "Astro Summarizer",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-summarizer.png",
+    },
+    {
+      title: "Astro Portfolio",
+      isFeatured: false,
+      thumbnail: "/assets/images/astro-portfolio.png",
+    },
+  ],
+};
