01f5949feebb170bf0d37de5bea0cd6017d302fd
r_g7d <danangr123@gmail.com>
2023-06-26 21:01:36 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://r-G7D.github.io",
-  base: "/",
+  base: "/astro-portfolio",
   integrations: [
     tailwind(),
     sitemap({
